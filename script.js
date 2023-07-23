2c9392490631252c967566d6873fea77e100fd2b
Viktoriia Ustymova <svi.3481@gmail.com>
2023-07-23 15:02:43 +0200

    update

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-const showImageButton  = document.getElementById("showImage");
-const image = document.getElementById("image");
+const showImageButton  = document.getElementById('showImage');
+const image = document.getElementById('image');
+
 const url ="https://dog.ceo/api/breeds/image/random";
+showImageButton.addEventListener("click", loadImage);
 
 function loadImage() {
-        let response= fetch(url);
-        response.then((response)=>{
-        data = response.json();
-        data.then((res)=>{
-            console.log(res);   
-         })
-        image.src = data.message;
-    });  
-    
+    fetch(url).then((response)=>{
+        if(response.ok){
+            return response.json();
+        } else {
+            return Promise.reject(new Error(response.status +" " + response.statusText));
+        }
+    }
+    ).then((res)=>{
+        if(res.status == "success" ) {
+            image.src=res.message;
+        } else {
+            return Promise.reject(new Error("something goes wrong! Status "+ res.status));
+        }
+    }).catch ( (qwer)=>{
+        console.log(qwer);  
+     }
+ );
+
 }
 
-showImageButton.addEventListener("click", () => {
-    loadImage();
-});
+
 
